9bbb7ba8950c9fdd6292b59d44084c08db0fc2c3
chad0909 <chad0909@naver.com>
2022-07-17 01:05:42 +0900

    BaseViewController Made (Background Color Made)

---
+++ Configuration/Base/BaseViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BaseViewController.swift
+//  RCtest-BunJangApp
+//
+//  Created by 077tech on 2022/07/17.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    // Background Color
+        self.view.backgroundColor = .white
+    }
+}
