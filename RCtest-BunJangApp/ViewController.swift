01fa91e99ee65f5589e656c1ce6b04a7486ec43a
chad0909 <chad0909@naver.com>
2022-07-16 14:03:18 +0900

    Initial Commit

---
+++ RCtest-BunJangApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RCtest-BunJangApp
+//
+//  Created by 077tech on 2022/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
