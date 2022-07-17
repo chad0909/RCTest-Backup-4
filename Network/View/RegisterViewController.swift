445a8a64be3de9ffa701be004280f7927f2097da
chad0909 <chad0909@naver.com>
2022-07-17 18:27:00 +0900

    Register bottom part finished (Safe & Register)

--- Network/View/RegisterViewController.swift
+++ Network/View/RegisterViewController.swift
@@ -9,21 +9,41 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var registerView: UIView!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var safeButton: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.tabBarController?.tabBar.isHidden = true
+        
+        UIUpdate()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    func UIUpdate(){
+        //모서리 굴곡률
+        safeButton.layer.cornerRadius = 10
+        //테두리 굵기
+        safeButton.layer.borderWidth = 0.8
+        //테두리 색상
+        safeButton.layer.borderColor = UIColor.gray.cgColor
+        
+        //모서리 굴곡률
+        registerButton.layer.cornerRadius = 10
+        //테두리 굵기
+        registerButton.layer.borderWidth = 0.8
+        //테두리 색상
+        registerButton.layer.borderColor = UIColor.red.cgColor
+        registerButton.semanticContentAttribute = .forceRightToLeft
+        
+        //테두리 굵기
+        registerView.layer.borderWidth = 0.4
+        //테두리 색상
+        registerView.layer.borderColor = UIColor.lightGray.cgColor
+        
     }
-    */
-
 }
