44188549664282b0a184865b77592445fa957b6c
chad0909 <chad0909@naver.com>
2022-07-17 23:23:28 +0900

    Register Screen Button & collectionView Edit needed.

---
+++ Network/Cell/RegisterCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterCollectionViewCell.swift
+//  RCtest-BunJangApp
+//
+//  Created by 077tech on 2022/07/17.
+//
+
+import UIKit
+
+class RegisterCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imgView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Network/View/RegisterViewController.swift
+++ Network/View/RegisterViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var topCollectionView: UICollectionView!
+    @IBOutlet weak var productTextField: UITextField!
+    @IBOutlet weak var categoryTextField: UILabel!
+    @IBOutlet weak var tagTextField: UILabel!
+    @IBOutlet weak var optionButton: UIButton!
+    
+    @IBOutlet weak var wonTextField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    //registerTab
     @IBOutlet weak var registerView: UIView!
     @IBOutlet weak var registerButton: UIButton!
     @IBOutlet weak var safeButton: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,31 +30,94 @@ class RegisterViewController: UIViewController {
         self.tabBarController?.tabBar.isHidden = true
         
         UIUpdate()
+        placeholderSetting()
+        registerXib()
+
+        topCollectionView.dataSource = self
+        topCollectionView.delegate = self
     }
     
 
     
     
     func UIUpdate(){
-        //모서리 굴곡률
         safeButton.layer.cornerRadius = 10
-        //테두리 굵기
         safeButton.layer.borderWidth = 0.8
-        //테두리 색상
         safeButton.layer.borderColor = UIColor.gray.cgColor
         
-        //모서리 굴곡률
         registerButton.layer.cornerRadius = 10
-        //테두리 굵기
         registerButton.layer.borderWidth = 0.8
-        //테두리 색상
         registerButton.layer.borderColor = UIColor.red.cgColor
         registerButton.semanticContentAttribute = .forceRightToLeft
         
-        //테두리 굵기
         registerView.layer.borderWidth = 0.4
-        //테두리 색상
         registerView.layer.borderColor = UIColor.lightGray.cgColor
         
+        productTextField.borderStyle = .none
+        productTextField.setPlaceholderColor(.systemGray)
+        
+        wonTextField.borderStyle = .none
+        wonTextField.setPlaceholderColor(.systemGray)
+        
+        optionButton.layer.cornerRadius = 5
+        optionButton.layer.borderWidth = 0.4
+        optionButton.layer.borderColor = UIColor.lightGray.cgColor
+        
     }
+    
+    
+    private func registerXib() {
+        let nibName = UINib(nibName: "RegisterCollectionViewCell", bundle: nil)
+        topCollectionView.register(nibName, forCellWithReuseIdentifier: "registerCell")
+    }
+    
+}
+
+public extension UITextField {
+    
+    func setPlaceholderColor(_ placeholderColor: UIColor) {
+        attributedPlaceholder = NSAttributedString(
+            string: placeholder ?? "",
+            attributes: [
+                .foregroundColor: placeholderColor,
+                .font: font
+            ].compactMapValues { $0 }
+        )
+    }
+    
+}
+
+extension RegisterViewController : UITextViewDelegate{
+    
+    func placeholderSetting(){
+        textView.delegate = self
+        textView.text = "여러 장의 상품사진과 구입 연도, 브랜드, 사용감, 하자 유무등 구매자에게 필요한 정보를 꼭 포함해 주세요. 문의를 줄이고 더 쉽데 판매할 수 있어요. (10자 이상)"
+        textView.textColor = UIColor.lightGray
+    }
+    func textViewDidBeginEditing(_ textView: UITextView){
+        if textView.textColor == UIColor.lightGray{
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    func textViewDidEndEditing(_ textView: UITextView){
+        if textView.text.isEmpty{
+            textView.text = "여러 장의 상품사진과 구입 연도, 브랜드, 사용감, 하자 유무등 구매자에게 필요한 정보를 꼭 포함해 주세요. 문의를 줄이고 더 쉽데 판매할 수 있어요. (10자 이상)"
+            textView.textColor = UIColor.lightGray
+        }
+    }
+}
+
+extension RegisterViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 13
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = topCollectionView.dequeueReusableCell(withReuseIdentifier: "registerCell", for: indexPath) as! RegisterCollectionViewCell
+        
+        return cell
+    }
+    
+   
 }
