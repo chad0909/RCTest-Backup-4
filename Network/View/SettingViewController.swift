563b0edc5d98cb4aede8e6aee16c1a8d6a3c75b9
chad0909 <chad0909@naver.com>
2022-07-19 04:33:17 +0900

    SettingViewController frame finished

---
+++ Network/View/SettingViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingViewController.swift
+//  RCtest-BunJangApp
+//
+//  Created by 077tech on 2022/07/19.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
