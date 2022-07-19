e28014b81213deb438870bc7b2bec554e2b96f5a
Gi Wook Lee <108044639+chad0909@users.noreply.github.com>
2022-07-20 06:48:02 +0900

    Following, Follower Screen made

---
+++ Network/View/FollowerViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FollowerViewController.swift
+//  RCtest-BunJangApp
+//
+//  Created by 077tech on 2022/07/20.
+//
+
+import UIKit
+
+class FollowerViewController: UIViewController {
+
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
